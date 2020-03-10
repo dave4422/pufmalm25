71b96ac91e8b1f8deafda203f749871798e96906
David Mildenberger <dave4422@gmail.com>
2020-03-10 02:13:16 +0100

    make model saller bc of memory requirements

--- few_shot/models.py
+++ few_shot/models.py
@@ -275,17 +275,17 @@ class FewShotClassifierPUF(nn.Module):
             nn.ReLU()
             )
         self.layer3 = nn.Sequential(
-            nn.Linear(in_features, 640),
+            nn.Linear(in_features, 320),
             torch.nn.Dropout(0.2),
             nn.ReLU()
             )
         self.layer4 = nn.Sequential(
-            nn.Linear(640, 320),
+            nn.Linear(320, 160),
             torch.nn.Dropout(0.3),
             nn.ReLU()
             )
         self.layer5 = nn.Sequential(
-            nn.Linear(320, in_features),
+            nn.Linear(160, in_features),
             torch.nn.Dropout(0.2),
             nn.ReLU()
             )
